1cbc0b5834fdf0a21a4ba564ee7be517af5398d2
romerofernando <romero_f22@yahoo.com>
2021-02-21 19:29:16 -0800

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,7 +1,9 @@
 // ASSESSMENT 2: Coding practical questions with Jest
 
 // Please read all questions thoroughly
+
 // Pseudo coding is HIGHLY recommended
+
 // If you get stuck, please leave comments to help us understand your thought process
 
 // Add appropriate dependencies to the repository:
@@ -27,16 +29,72 @@ var num3 = -7
 // Expected output: "-7 is not divisble by three"
 
 
+// describe ("When divisibleByThree gets called, return if value can be evenly devided by 3", () =>{
+//     it (`if number is evenly divisible by 3, return ${argument} is divisible by 3`, () => {
+//         const argument = 15;
+//         const actualResults = divisibleByThree(argument)
+//         expect(actualResults).toEqual(`${argument} is divisible by 3`)
+//     })
+//     it (`if number is evenly divisible by 3, return ${argument} is divisible by 3`, () => {
+//         const argument = 0;
+//         const actualResults = divisibleByThree(argument)
+//         expect(actualResults).toEqual(`${argument} is divisible by 3`)
+//     })
+//      it (`if number is not evenly divisible by 3, return ${argument} is not divisible by 3`, () => {
+//             const argument = -7;
+//             const actualResults = divisibleByThree(argument)
+//             expect(actualResults).toEqual(`${argument} is not divisible by 3`)
+//     })
+// })
+
+const divisibleByThree= (value) =>{
+    if(value % 3 !== 0){
+        return `${value} is not divisible by 3`
+    }else if(value % 3 === 0){
+        return `${value} is divisible by 3`
+    }else
+    return `${value} is not divisible by 3`
+}
+
+var num1 = 15
+
+var num2 = 0
+
+var num3 = -7
+
+console.log(divisibleByThree(num1))
+console.log(divisibleByThree(num2))
+console.log(divisibleByThree(num3))
 
 
-
-// b) Create the function that makes the test pass.
 
 
 
 
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
+
+
+//creating function that takes in an array 
+//retruns new array .map
+// index of 0 toUpperCase
+
+// test ("when function capitalize is called, return will retrun a new array with first letter capitalize", () =>{
+//     const argument = randomNouns1
+//     const actualResults = cappedWord(argument)
+//     expect (actualResults).toEqual["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+// })
+
+
+const capitalize = (array) => { 
+    let cappedWord =[]
+    array.forEach(element => {
+        cappedWord.push (element[0].toUpperCase() + element.slice (1,element.length));
+    })
+    return cappedWord.join(" ")
+}
+
+
 
 // a) Create a test with expect statements for each of the variables provided.
 
@@ -46,6 +104,8 @@ var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
 
+console.log(capitalize(randomNouns1))
+console.log(capitalize(randomNouns2))
 
 
 
@@ -58,16 +118,35 @@ var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction
 
 // --------------------3) Create a function that takes in an array of mixed data types and returns an array with ONLY NUMBERS sorted from least to greatest.
 
+// creating function onlyNumbers
+//takes mixed array as function 
+//returns only numbers
+//create a test that take function as the argument and check if it retruns numbers
+
+// test ("when function onlyNumbers is called, return a new array with only numbers", () =>{
+//     const argument = mixedDataArray1
+//     const actualResults = newArray(argument)
+//     expect (actualResults).toEqual["-8","0", "8", "46", "59", "107"]
+// })
+
+const onlyNumbers = (array) => {
+    let newArray = array.filter(value =>{
+        return typeof value === "number"
+    })
+    return newArray
+}
+
 // a) Create a test with expect statements for each of the variables provided.
 
 var mixedDataArray1 = [true, 8, "hello", -8, null, 0, 46, 59, 107, "hey!"]
 // Expected output: [-8, 0, 8, 46, 59, 107]
 
-var mixedDataArray1 = [3, "yo!", 94, -9, false, 0, 18, "hola!"]
+var mixedDataArray2 = [3, "yo!", 94, -9, false, 0, 18, "hola!"]
 // Expected output: [-9, 0, 3, 18, 94]
 
 
-
+console.log(onlyNumbers(mixedDataArray1))
+console.log(onlyNumbers(mixedDataArray2))
 
 
 // b) Create the function that makes the test pass.
@@ -80,13 +159,63 @@ var mixedDataArray1 = [3, "yo!", 94, -9, false, 0, 18, "hola!"]
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var vowelTester1 = "learn"
-// Expected output: 1
-var vowelTester2 = "academy"
-// Expected output: 0
-var vowelTester3 = "challenge"
-// Expected output: 2
+//create a function that takes a string as argument
+//returns index of first vowel
 
+//newArray = string.split() to split the string into a new array
+//use foreach to run throught the vlues of the array and retrun the indexof(value)
+
+////????? i finally gave up i have spend most of my day trying to make this work. 
+
+//?????i can make it five me the location of the vowels at one point i also had it giving pushing the first vowel to the new array but i I counlnt get it give me the location of the index 
+
+//????the .forEach was my last ditch effort to make it work. 
+
+// describe("When firsVowlIndex gets called, return only the index of the first vowel in string")
+// const argument = vowlTester1
+// const actualResults = firstLett(argument)
+// expect(actualResults).teEqual("1")
+
+
+// const firstVowleIndex = (string) => {
+//     let newArray = string.split('');
+//     // let firstLett =[]
+//     let firstVowel = newArray.indexOf('a' || 'e' ||'i' || 'o'|| 'u' || 'y')
+//   // return firstVowel.forEach(value => {
+//   //   if ('a' > 'e','i','o','u')
+//   //   return firstLett.push(value);
+//   //   else if('e' > 'a','i','o','u')
+//   //     return firstLett.push(value);
+//   //       else if('i' > 'a','i','o','u')
+//   //     return firstLett.push(value);
+//   //       else if('o' > 'a','i','o','u')
+//   //     return firstLett.push(value);
+//   //       else if('u' > 'a','i','o','u')
+//   //     return firstLett.push(value);
+//   //     })  
+//     //   return firstLett
+//     // }
+
+//   return firstVowel
+
+//     }   
+//   // for(let i = 0 ; i < array.length; i++)
+//     // { 
+// // }
+// // if ('a' > 'e','i','o','u')
+// // return
+  
+
+// var vowelTester1 = "learn"
+// Expected output: 1
+
+// var vowelTester2 = "academy"
+// Expected output: 0
+// var vowelTester3 = "challenge"
+// Expected output: 3
+// console.log(firstVowleIndex(vowelTester1))
+// console.log(firstVowleIndex(vowelTester2))
+// console.log(firstVowleIndex(vowelTester3))
 
 
 
--- interview-questions.md
+++ interview-questions.md
@@ -8,49 +8,57 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. Why would you use the method super()?
 
-  Your answer:
+  Your answer: I would use the super() method when I am wanting to inherit an atribute for an object. 
 
   Researched answer:
-
+super() is used to call the parent constructor. super(props) would pass props to the parent constructor. From your example, super(props) would call the React. Component constructor passing in props as the argument.
 
 
 2. What is a virtual DOM?
 
-  Your answer:
+  Your answer: A virtual DOM is created when using react, this listens to changes made through the page and updates them without needed to refresh the entire page. 
 
   Researched answer:
+  The virtual DOM (VDOM) is a programming concept where an ideal, or “virtual”, representation of a UI is kept in memory and synced with the “real” DOM by a library such as ReactDOM. This process is called reconciliation. ... They may also be considered a part of “virtual DOM” implementation in React. Like the actual DOM, the Virtual DOM is a node tree that lists elements and their attributes and content as objects and properties. React's render() method creates a node tree from React components and updates this tree in response to mutations in the data model, caused by actions
 
 
 
 3. When creating a basic (stateless) class component in React, what are the necessary elements needed to display "Hello World" in the browser?
 
-  Your answer:
+  Your answer: you would need a render tag a return tag and anything you want visible one the page  would be placed between <> </>
 
   Researched answer:
+Create new Header component and type out the below code into Header.js. src\components\Header.js. 
+Create new Footer component and type out the below code into Footer.js. src\components\Footer.js. 
+Now, update default App.js with below code snippet. src\App.js.
 
 
 
 4. What is JSX?
 
-  Your answer:
+  Your answer: JSX is a language used by react to integrate HTML and javascript
 
   Researched answer:
+  JSX stands for JavaScript XML it allows us to write a HTML in React making it easier to write and add HTML in react
 
 
 
 5. What is state in React?
 
-  Your answer:
+  Your answer: State holds the information for the app to use. 
 
   Researched answer:
-
+React components has a built-in state object. The state object is where you store property values that belongs to the component. When the state object changes, the component re-renders.
 
 
 6. STRETCH: What is hoisting in JavaScript?
 
-  Your answer:
+  Your answer: I dont know yet but will look into it and get the answer 
+
+  From what I read it sounds like a the var or declared values are stored at the top of the file but still can not be iniciated until it is read through the line. This may be helpful when multiple declared values are used. 
 
   Researched answer:
+  Hoisting is JavaScript's default behavior of moving all declarations to the top of the current scope (to the top of the current script or the current function).
 
 
 
@@ -59,7 +67,21 @@ Then, research the question to expand on your answer. Even if you feel you have 
 Research and define the following terms to the best of your ability.
 
 - React lifecycle methods
+A component in React that is monitored and manipulated during the three main phases. Mounting Updating and Unmounting
+
 - React props
+react props are like functions in javascript 
+
+that pass arguments into react components
+
 - event.preventDefault()
+
+A method that that cancels out a defualt action when it is able to be cancelable
+
 - DOM events
+This allows javascript to register different event handlers on elements in an HTML document. 
+
 - MVC
+
+Model-view-controller
+a software design pattern used to develop user interfaces that divides the realted program into three interconnected elements. 
